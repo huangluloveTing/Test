7ca6ee4fd87254675f2a0726d1478fb3dda78d6f
黄露 <huanglu@huangluMacPro.local>
2017-01-24 16:38:48 +0800

    commit

---
+++ PBTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PBTest
+//
+//  Created by Caichao on 16/1/16.
+//  Copyright © 2016年 Caichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PBTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PBTest
+//
+//  Created by Caichao on 16/1/16.
+//  Copyright © 2016年 Caichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
